704a1aa693c3d1f0aa3edd909bf95bcdcf4b6e5e
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2025-09-17 14:49:25 +0700

    Cau_hinh_lại_folder_pug

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,5 @@
- module.exports.index = (req, res) => {  // Route trang chủ
-   res.render('client/pages/home/index.pug'); // res.render là hiển thị giao hiện từ file .pug, đường dẫn vào sẵn trong thư mục view rồi nên không cần phải view/client/... nữa
+module.exports.index = (req, res) => { // Route trang chủ
+  res.render('client/pages/home/index.pug', { // res.render là hiển thị giao hiện từ file .pug, đường dẫn vào sẵn trong thư mục view rồi nên không cần phải view/client/... nữa
+    pageTitle: "Trang chủ" // cái này để hiện thị tiêu đề trên thanh tab
+  });
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,5 @@
  module.exports.index = (req, res) => {  // Route trang chủ
-   res.render('client/pages/home/index.pug'); // res.render là hiển thị giao hiện từ file .pug, đường dẫn vào sẵn trong thư mục view rồi nên không cần phải view/client/... nữa
+   res.render('client/pages/products/index', {
+    pageTitle :"Danh sách sản phẩm" // cái này để hiện thị tiêu đề trên thanh tab
+   }); // res.render là hiển thị giao hiện từ file .pug, đường dẫn vào sẵn trong thư mục view rồi nên không cần phải view/client/... nữa
 }
