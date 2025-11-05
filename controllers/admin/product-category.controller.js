0f836f6f06b6e09262126e51d96828939e3f5e25
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2025-11-05 23:40:09 +0700

    Xây dựng phần quản lý danh mục sản phẩm (phần 2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,21 +1,31 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system.js")
+const createTreeHelper = require("../../helpers/createTree.js")
 // [GET]: /admin/product-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false
   };
   const records = await ProductCategory.find(find)
+  const newRecords = createTreeHelper.tree(records)
+
   res.render('admin/pages/products-category/index', {
     pageTitle: "Danh mục sản phẩm",
-    records: records
+    records: newRecords
   });
 };
 
 // [GET]: /admin/product-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted : false
+  }
+  const records = await ProductCategory.find(find).sort({position:"asc"})
+  const newRecords = createTreeHelper.tree(records)
+  console.log(newRecords)
   res.render('admin/pages/products-category/create', {
     pageTitle: "Tạo danh mục sản phẩm",
+    records : newRecords
   });
 };
 
