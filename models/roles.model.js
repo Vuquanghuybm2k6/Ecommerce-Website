ef10b78a1e02a625d3de1b166976ac2af88ce713
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2025-11-07 01:16:30 +0700

    Xây dựng nhóm quyền và tạo collection roles trong database

---
+++ models/roles.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const roleSchema = new mongoose.Schema({
+  title: String, 
+  product_category_id: {
+    type: String,
+    default: ""
+  },
+  description: String,
+  permission: {
+    type:Array,
+    default: []
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date // thêm thời gian xóa vào lúc nào
+}, {
+  timestamps: true
+});
+const Role = mongoose.model("Product", roleSchema, "products"); // tham số thứ 3 "products" là cái tên connection trong database
+module.exports = Role;
