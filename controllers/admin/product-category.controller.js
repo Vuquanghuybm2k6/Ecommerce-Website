dc6c304cd70e22aaf937fa040507de4ff98e494c
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2025-11-06 18:06:14 +0700

    Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -29,7 +29,7 @@ module.exports.create = async (req, res) => {
   });
 };
 
-//[POST] : /admin/product-category/create
+//[POST]: /admin/product-category/create
 module.exports.createPost = async (req, res) => {
   if (req.body.position == "") {
     const countProducts = await ProductCategory.countDocuments()
@@ -41,3 +41,29 @@ module.exports.createPost = async (req, res) => {
   await record.save()
   res.redirect(`${systemConfig.prefixAdmin}/products-category`)
 };
+
+// [GET]: /admin/product-category/edit/:id
+module.exports.edit = async (req,res) =>{
+  try{
+    const id = req.params.id
+  const data = await ProductCategory.findOne({_id:id, deleted:false})
+  const records = await ProductCategory.find({deleted:false})
+  const newRecords = createTreeHelper.tree(records)
+  res.render('admin/pages/products-category/edit',{
+    pageTitle: "Chỉnh sửa danh mục sản phẩm",
+    data:data,
+    records : newRecords
+  })
+  }
+  catch{
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+  }
+}
+
+// [PATCH]: /admin/product-category/edit/:id
+module.exports.editPatch = async (req,res) =>{
+  const id = req.params.id
+  req.body.position = parseInt(req.body.position)
+  await ProductCategory.updateOne({_id:id},req.body)
+  res.redirect(req.get("Referer"))
+}
