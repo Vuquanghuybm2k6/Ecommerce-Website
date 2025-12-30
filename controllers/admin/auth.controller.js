fe536f09a3bb545c5128cb6492a45cd0433bebe2
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2025-12-31 00:32:00 +0700

    Làm tính năng đăng nhập

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,35 @@
+const Account = require("../../models/account.model")
+const md5 = require('md5')
+const systemConfig = require("../../config/system.js")
+// [GET]: /admin/auth/login
+module.exports.login = async(req,res)=>{
+  res.render("admin/pages/auth/login",{
+    pageTitle: "Trang đăng nhập",
+  })
+  
+}
+// [POST]: /admin/auth/loginPost
+module.exports.loginPost = async(req,res)=>{
+  const email  = req.body.email
+  const password = req.body.password
+  const user = await Account.findOne({
+    email : email,
+    deleted: false
+  })
+  if(!user){
+    req.flash("error", "Email không tồn tại")
+    res.redirect(req.get("Referer"))
+  }
+  console.log(password)
+  if(md5(password) != user.password){
+    req.flash("error", "Sai mật khẩu")
+    res.redirect(req.get("Referer"))
+  }
+  if(user.status == "inactive"){
+    req.flash("error", "Tài khoản đã bị khóa")
+    res.redirect(req.get("Referer"))
+    return
+  }
+  res.cookie("token", user.token)
+  res.redirect(`${systemConfig.prefixAdmin}/dashboard`)
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productRoutes = require("./product.route")
 const productCategoryRoutes = require("./product-category.route")
 const roleRoutes = require("./role.route")
 const accountRoutes = require("./account.route")
+const authRoutes = require("./auth.route")
 module.exports = (app) => { 
     const PATH_ADMIN = systemConfig.prefixAdmin; // khai báo tách riêng ra như này để nếu mà người dùng muốn đổi tên thì chỉ cần đổi ở dòng này
     app.use( PATH_ADMIN + "/products-category", productCategoryRoutes); 
@@ -11,4 +12,5 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/products", productRoutes)
     app.use(PATH_ADMIN + "/roles", roleRoutes)
     app.use(PATH_ADMIN + "/accounts", accountRoutes)
+    app.use(PATH_ADMIN + "/auth",authRoutes )
 }
