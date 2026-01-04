ac079efc477ac7b0a615d5992c79a450f511f94f
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-01-04 22:00:10 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -7,11 +7,22 @@ module.exports.index =async (req, res) => {
     featured : "1",
     deleted: false,
     status: "active"
-  })
-  const newProducts = productsHelper.priceNewProducts(productsFeatured)
+  }).limit(6)
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured)
   // End Lấy ra sp nổi bật
+
+  // Hiển thị ds sp mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active"
+  }).sort({position:"desc"}).limit(6)
+  const newProductsNew = productsHelper.priceNewProducts(productsNew)
+
+  // End Hiển thị ds sp mới nhất
+
   res.render('client/pages/home/index.pug', { // res.render là hiển thị giao hiện từ file .pug, đường dẫn vào sẵn trong thư mục view rồi nên không cần phải view/client/... nữa  
     pageTitle: "Trang chủ", // cái này để hiện thị tiêu đề trên thanh tab
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew : newProductsNew
   });
 }
