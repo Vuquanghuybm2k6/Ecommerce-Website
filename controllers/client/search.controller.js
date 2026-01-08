6b59a79afb8fe97baf68d67a4e11fccd2aed46e8
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-01-08 11:09:21 +0700

    Làm thanh tìm kiếm bên phía client

---
+++ controllers/client/search.controller.js
@@ -0,0 +1,22 @@
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
+//  [GET] /search
+module.exports.index = async (req, res) => {
+  const keyword = req.query.keyword
+  let newProducts = [] // khai báo newProducts bên ngoài để có thế truyền vào trong cái hàm res.render
+  if(keyword){
+    const keywordRegex = new RegExp(keyword, "i")
+    const products = await Product.find({
+      title: keywordRegex,
+      status : "active",
+      deleted: false
+    })
+    newProducts = productsHelper.priceNewProducts(products)
+    console.log(newProducts)
+  }
+  res.render('client/pages/search/index', {
+    pageTitle: "Kết quả tìm kiếm",
+    keyword: keyword,
+    products : newProducts
+})
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,13 +1,15 @@
 const productRouters = require("./product.route") //Import route riêng của phần sản phẩm từ file product.route.js
 const homeRouters = require("./home.route") //Import route riêng của phần sản phẩm từ file home.route.js
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
+const searchRouters = require("./search.route")
 module.exports = (app) => { // Export ra một hàm nhận tham số là app (chính là biến const app = express() trong file index.js )
+   app.use(categoryMiddleware.category) // cái này sử dụng để đỡ phải gọi nhiều lần cái app.use('/products', categoryMiddleware.category, productRouters);, sau này có nhiều trang sử dụng cái này thì chỉ cần gọi 1 lần như này là được
    // Khi người dùng truy cập vào '/' (trang chủ),
     // thì Express sẽ sử dụng các route được định nghĩa trong homeRouters
     app.use('/', homeRouters);
     // Khi người dùng truy cập vào '/products',
     // thì Express sẽ sử dụng các route được định nghĩa trong productRouters
     // Ví dụ: /products/list hoặc /products/detail
-    app.use(categoryMiddleware.category) // cái này sử dụng để đỡ phải gọi nhiều lần cái app.use('/products', categoryMiddleware.category, productRouters);, sau này có nhiều trang sử dụng cái này thì chỉ cần gọi 1 lần như này là được
     app.use('/products', categoryMiddleware.category, productRouters); 
+    app.use('/search',searchRouters);
 }
