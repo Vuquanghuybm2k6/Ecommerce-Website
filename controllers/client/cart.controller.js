f2868ea2bd689479ecf292ecfdd9132ee3dfa4a1
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-01-10 11:37:23 +0700

    Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,6 +1,31 @@
 // Import model Cart để thao tác với collection giỏ hàng trong database
 const Cart = require("../../models/cart.model")
-
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
+// [GET]: /cart
+module.exports.index = async (req,res) =>{
+  const cartId = req.cookies.cartId
+  const cart = await Cart.findOne({
+    _id: cartId
+  })
+  if(cart.products.length > 0){
+    for(const item of cart.products){ // tìm bản ghi trong db Product để lấy ra cái nào có id giống với id sản phẩm trong giỏ hàng
+      const productId = item.product_id
+      const productInfo = await Product.findOne({
+        _id : productId
+      })
+      productInfo.priceNew = productsHelper.priceNewProduct(productInfo) // tìm giá mới sau khi giảm
+      item.productInfo = productInfo 
+      item.totalPrice = item.quantity*productInfo.priceNew // tính tổng tiền của đơn hàng bằng cách nhân giá tiền với số lượng đơn
+    }
+  }
+  cart.totalPrice = cart.products.reduce((sum,item)=>sum+item.totalPrice,0)
+  console.log(cart)
+  res.render("client/pages/cart/index",{
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  })
+}
 // [POST] /cart/add/:productId
 // Controller xử lý thêm sản phẩm vào giỏ hàng
 module.exports.addPost = async (req, res) => {
